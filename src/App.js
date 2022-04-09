3f4413a1ad0a15810a7fde5cb3ef66aed197286d
shawasty <shawasty@gmail.com>
2022-04-08 21:50:56 -0400

    called api with useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { fetchUser } from './services/rando_user';
+import { useEffect, useState } from 'react'
 import './App.css';
 import Header from './components/Header';
 
 function App() {
+  const [currentUser, setCurrentUser]= useState(null)
+
+  useEffect(()=>{
+    refreshUser()
+  },[])
 
   const refreshUser =(e) => {
-    console.log('clicked')
-    
+    // e.preventDefault()
+    fetchUser().then(res=>{
+      setCurrentUser(res)
+      console.log('curr', currentUser)
+    }).catch(err =>(err))
   }
 
 
--- src/services/rando_user.jsx
+++ src/services/rando_user.jsx
@@ -3,7 +3,14 @@ import axios from 'axios';
 const base_url ="https://api.randomuser.me/";
 
 export async function fetchUser(){
+    try{
     const response = await axios.get(base_url);
-    console.log ('res..', response)
+    const result = response.data.results
+    console.log ('res..', result)
+    return result;
+
+    }catch(error){
+        console.log(error)
+    }
 }
 fetchUser()
